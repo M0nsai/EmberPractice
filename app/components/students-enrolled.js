3ac7de988f46323f3784ffcbbc747da24d4021b0
DESKTOP-A5CIQM9\saygo <saygonza23@gmail.com>
2023-07-31 01:08:25 -0600

    Modal for viewing the students enrolled, that include a change of color on the buttom if there´s no students enrolled. (npm install required)

---
+++ app/components/students-enrolled.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class StudentsEnrolled extends Component {
+
+  @tracked isOpen = false;
+
+}
